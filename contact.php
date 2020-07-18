46d45d265e985c9596600c0c42e7683aa9cdc412
nomanfahad <nomanfahad7@gmail.com>
2020-07-18 13:29:10 +0600

    contact us update

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+    $name =$_POST['name'];
+    $number =$_POST['number'];
+    $mailfrom =$_POST['mail'];
+    $message =$_POST['message'];
+
+
+    $mailto = "nomanfahad7@gmail.com";
+    $headers = "From: ".$mailfrom;
+    $txt = "You have recieved an E-mail from ".$name.".\n\n".$message;
+
+
+
+    mail($mailto, $number, $txt, $headers);
+    header("Location: index.html?mailsend");
+    
+
+}
+
+
+?>
